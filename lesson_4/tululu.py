b6d89cd228298d4544efdc4ba2bffc995f808216
Nikita <bigmama93@mail.ru>
2022-03-22 14:31:33 +0300

    парсит первые 10 страниц с книгами по жанру

--- lesson_4/tululu.py
+++ lesson_4/tululu.py
@@ -3,16 +3,22 @@ import requests
 from pathlib import Path
 from urllib.parse import urljoin, urlparse
 
-
+#
 def parse_book_category():
     category_url = "https://tululu.org/l55/"
     response = requests.get(category_url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    find_book = soup.find_all('table', class_='d_book')
-    for book in find_book:
-        id_book = book.find('a').get('href')
-        url_book = urljoin(category_url, id_book)
-        print(url_book)
+    for i in range(1,11):
+        url_book = urljoin(category_url, str(i))
+        response = requests.get(url_book)
+        soup = BeautifulSoup(response.text, 'lxml')
+        find_book = soup.find_all('table', class_='d_book')
+        for book in find_book:
+            id_book = book.find('a').get('href')
+            url_book = urljoin(category_url, id_book)
+            print(url_book)
 
 
 parse_book_category()
+
+# url = urlparse("https://tululu.org/l55/3/")
+# print(url)
